4d2541332a4452ea1e5cfee4af18ff4fab18ab2e
daehwan2 <dhdh0482@naver.com>
2023-03-02 22:38:37 +0900

    Chore: 라우터 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './container';
+import Mypage from './container/mypage';
+import Login from './container/login';
+import Gym from './container/gym';
+import Owner from './container/owner';
+import Signup from './container/signup';
 
 function App() {
-  return <div className="App">Basketball Indoor Gym Service</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="mypage" element={<Mypage />} />
+        <Route path="owner" element={<Owner />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="gym" element={<Gym />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
