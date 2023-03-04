78339b9c5eb78e0d35b19e8c8d2823a96bde208a
daehwan2 <dhdh0482@naver.com>
2023-03-04 16:58:58 +0900

    Chore: 공통으로 사용할 컬러, 폰트 정의

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 
-import App from "./App";
+import App from './App';
+import GlobalStyle from './style';
+import { theme } from './style/theme';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
