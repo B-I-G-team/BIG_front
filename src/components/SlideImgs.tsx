687aca1fdb60f7f4bf3bc77210e47603a125a490
Kim minseok <80530044+AdolphKim@users.noreply.github.com>
2023-03-18 23:23:58 +0900

    Style: Slide 컴포넌트 생성 (#9)
    * 파일 생성
    
    * 혼나고 다시 만듬
    
    * slide 임시 작업중
    
    * Slide 기능 적용
    
    * 슬라이드 컴포넌트 위치 수정
    
    * 안쓰는 라이브러리 제거
    
    * 팀대관 텍스트 수정
    
    * 슬라이드 피그마에 맞게 수정
    
    * 안쓰이는 파일 삭제
    
    * 안쓰이는 컴포넌트 삭제
    
    * 안쓰이는 라이브러리 삭제
    
    * 불필요 css 삭제
    
    ---------
    
    Co-authored-by: daehwan2 <dhdh0482@naver.com>

---
+++ src/components/SlideImgs.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  data: {
+    id: number;
+    image: string;
+  };
+}
+
+const SlideImgs = ({ data }: Props) => {
+  return (
+    <ImageBox>
+      <Image src={data.image} alt="image" />
+    </ImageBox>
+  );
+};
+
+export default SlideImgs;
+
+const ImageBox = styled.div`
+  cursor: pointer;
+
+  max-height: 450px;
+  @media ${({ theme }) => theme.grid.laptop} {
+    border-radius: 20px;
+  }
+  overflow: hidden;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: auto;
+`;
