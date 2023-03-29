65231a7c7e1def08458fd52ba3d60f52e7718773
AdolphKim <als981209@gmail.com>
2023-03-29 17:16:29 +0900

    prettier 적용

--- src/components/mypage/Mypage.tsx
+++ src/components/mypage/Mypage.tsx
@@ -1,152 +1,164 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import profileImg1 from 'assets/logo.png'
-import profileImg2 from 'assets/place1.jpeg'
+import profileImg1 from 'assets/logo.png';
+import profileImg2 from 'assets/place1.jpeg';
 import Reserve from './Reserve';
-const WAIT_CONFIRM = "wait_confirm"
-const WAIT_PAY = "wait_pay"
-const RESERVE = 'reserve'
-const TEAM = "team"
-const PICKUP = 'pickup'
+const WAIT_CONFIRM = 'wait_confirm';
+const WAIT_PAY = 'wait_pay';
+const RESERVE = 'reserve';
+const TEAM = 'team';
+const PICKUP = 'pickup';
 
 const data = [
-    {
-        id : 1,
-        img : profileImg1,
-        name : '김민석',
-        team : 'Start',
-        Roll : 'C',
-        height : 191,
-        weight : 91
-    },
-    {
-        id : 2,
-        img : profileImg2,
-        name : '김대환',
-        team : 'Freaks',
-        Roll : 'SG',
-        height : 170,
-        weight : 60
-    },
-    {
-        id : 3,
-        img : profileImg1,
-        name : '김민범',
-        team : '에어메카',
-        Roll : 'SF',
-        height : 175,
-        weight : 70
-    }
-  ];
-  
+  {
+    id: 1,
+    img: profileImg1,
+    name: '김민석',
+    team: 'Start',
+    Roll: 'C',
+    height: 191,
+    weight: 91,
+  },
+  {
+    id: 2,
+    img: profileImg2,
+    name: '김대환',
+    team: 'Freaks',
+    Roll: 'SG',
+    height: 170,
+    weight: 60,
+  },
+  {
+    id: 3,
+    img: profileImg1,
+    name: '김민범',
+    team: '에어메카',
+    Roll: 'SF',
+    height: 175,
+    weight: 70,
+  },
+];
+
 const Mypage = () => {
-  const [UserIndex , setIndex] = useState(0);
+  const [UserIndex, setIndex] = useState(0);
   const user = data[UserIndex];
   const menuArr = [
     { name: '픽업게임', content: PICKUP },
-    { name: '팀 대관', content: TEAM }
+    { name: '팀 대관', content: TEAM },
   ];
 
   const subMenuArr = [
-    { name: '승인 대기중', content : WAIT_CONFIRM},
-    { name: '결제 대기중', content : WAIT_PAY},
-    { name: '예약 완료' , content : RESERVE}
-  ]
+    { name: '승인 대기중', content: WAIT_CONFIRM },
+    { name: '결제 대기중', content: WAIT_PAY },
+    { name: '예약 완료', content: RESERVE },
+  ];
   const [currentTab, clickTab] = useState(0);
-  const [currentSubTab,clickSubTab] = useState(0);
+  const [currentSubTab, clickSubTab] = useState(0);
 
-  const selectMenuHandler = (index:number) => {
+  const selectMenuHandler = (index: number) => {
     clickTab(index);
     clickSubTab(0);
   };
 
-  const selectSubMenuHandler = (index:number) => {
+  const selectSubMenuHandler = (index: number) => {
     clickSubTab(index);
-  }
-  const userButtonClick = () =>{
-    setIndex(UserIndex+1)
-    if (UserIndex === 2){
+  };
+  const userButtonClick = () => {
+    setIndex(UserIndex + 1);
+    if (UserIndex === 2) {
       setIndex(0);
     }
-  }
+  };
   return (
     <Container>
-    <button onClick = {userButtonClick}>+ 유저 정보 바뀔시</button>
-    <Title>내 정보</Title>
-    <InformationBox>
-      <Image src={user.img} alt =""/>
-    <Informations>
-        <Information>이름 : {user.name}</Information>
-        <Information>소속팀: {user.team}</Information>
-        <Information>포지션: {user.Roll}</Information>
-        <Information>키 / 몸무게: {user.height} / {user.weight}</Information>
-    </Informations>
-    </InformationBox>
-    <Title>진행중인 예약정보</Title>
-    <TabMenu>
-    {menuArr.map((el,index) => (
-              <li key = {index} className={index === currentTab ? "submenu focused" : "submenu" }
-              onClick={() => selectMenuHandler(index)}>{el.name}</li>
-            ))}
-    </TabMenu>
-    <SubTab>
-    {subMenuArr.map((el,index) => (
-              <li key = {index} className={index === currentSubTab ? "submenu focused" : "submenu" }
-              onClick={() => selectSubMenuHandler(index)}>{el.name}</li>
-            ))}
-    </SubTab>
-    <Reserve Data = {menuArr[currentTab]} subData = {subMenuArr[currentSubTab]}/>
+      <button onClick={userButtonClick}>+ 유저 정보 바뀔시</button>
+      <Title>내 정보</Title>
+      <InformationBox>
+        <Image src={user.img} alt="" />
+        <Informations>
+          <Information>이름 : {user.name}</Information>
+          <Information>소속팀: {user.team}</Information>
+          <Information>포지션: {user.Roll}</Information>
+          <Information>
+            키 / 몸무게: {user.height} / {user.weight}
+          </Information>
+        </Informations>
+      </InformationBox>
 
+      <Title>진행중인 예약정보</Title>
+      <TabMenu>
+        {menuArr.map((el, index) => (
+          <li
+            key={index}
+            className={index === currentTab ? 'submenu focused' : 'submenu'}
+            onClick={() => selectMenuHandler(index)}
+          >
+            {el.name}
+          </li>
+        ))}
+      </TabMenu>
+      <SubTab>
+        {subMenuArr.map((el, index) => (
+          <li
+            key={index}
+            className={index === currentSubTab ? 'submenu focused' : 'submenu'}
+            onClick={() => selectSubMenuHandler(index)}
+          >
+            {el.name}
+          </li>
+        ))}
+      </SubTab>
+      <Reserve Data={menuArr[currentTab]} subData={subMenuArr[currentSubTab]} />
     </Container>
-  )
-}
+  );
+};
 
-export default Mypage
+export default Mypage;
 const Container = styled.div`
-  height : 150vh;
-`
+  height: 150vh;
+`;
 const InformationBox = styled.div`
-  display : flex;
-  align-items : center;
-  justify-content : center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   @media ${({ theme }) => theme.grid.tablet} {
-    justify-content :flex-start;
+    justify-content: flex-start;
   }
 `;
 
 const Title = styled.p`
-font-size: ${({ theme }) => theme.font.size.subtitle_1};
-font-weight: 600;
-padding: 10px 0;
-@media ${({ theme }) => theme.grid.tablet} {
-  font-size: ${({ theme }) => theme.font.size.heading_6};
-  font-weight: 700;
-}
+  font-size: ${({ theme }) => theme.font.size.subtitle_1};
+  font-weight: 600;
+  padding: 10px 0;
+  @media ${({ theme }) => theme.grid.tablet} {
+    font-size: ${({ theme }) => theme.font.size.heading_6};
+    font-weight: 700;
+  }
 
-@media ${({ theme }) => theme.grid.laptop} {
-  font-size: ${({ theme }) => theme.font.size.heading_5};
-}
+  @media ${({ theme }) => theme.grid.laptop} {
+    font-size: ${({ theme }) => theme.font.size.heading_5};
+  }
 `;
 
-const Informations =styled.ul`
-    list-style : none;
-    display :flex;
-    flex-direction : column;
-    align-items : flex-start;
+const Informations = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 `;
 
 const Information = styled.li`
-    padding : 10px;
+  padding: 10px;
 `;
 
 const Image = styled.img`
-  width : 100px;
-  height : 120px;
+  width: 100px;
+  height: 120px;
 `;
 
 const TabMenu = styled.ul`
   background-color: #dcdcdc;
-  padding : 0px;
+  padding: 0px;
   color: rgb(150, 150, 150);
   font-weight: bold;
   display: flex;
@@ -163,31 +175,29 @@ const TabMenu = styled.ul`
   }
 
   .focused {
-    background-color: rgb(255,255,255);
-    color: rgb(21,20,20);
+    background-color: rgb(255, 255, 255);
+    color: rgb(21, 20, 20);
   }
 `;
 
 const SubTab = styled.ul`
   font-weight: bold;
-  padding : 0px;
+  padding: 0px;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   list-style: none;
   @media ${({ theme }) => theme.grid.tablet} {
-    flex-direction : row;
-    
+    flex-direction: row;
   }
   .submenu {
-    color : #CDCFFF;
+    color: #cdcfff;
     display: flex;
     transition: 0.5s;
     @media ${({ theme }) => theme.grid.tablet} {
       width: 100px;
       padding: 10px;
       font-size: 15px;
-      
     }
     width: 30vw;
     padding: 10px;
@@ -195,6 +205,6 @@ const SubTab = styled.ul`
   }
 
   .focused {
-    color: #5D5FEF;
+    color: #5d5fef;
   }
 `;
--- src/components/mypage/Reserve.tsx
+++ src/components/mypage/Reserve.tsx
@@ -1,214 +1,213 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
 import placeImg from 'assets/place1.jpeg';
-const WAIT_CONFIRM = "wait_confirm"
-const WAIT_PAY = "wait_pay"
-const RESERVE = 'reserve'
-const TEAM = "team"
-const PICKUP = 'pickup'
+const WAIT_CONFIRM = 'wait_confirm';
+const WAIT_PAY = 'wait_pay';
+const RESERVE = 'reserve';
+const TEAM = 'team';
+const PICKUP = 'pickup';
 interface Props {
-    Data :{
-        name : string;
-        content : string;
-    }
-    subData : {
-        name : string;
-        content : string ;
-    }
+  Data: {
+    name: string;
+    content: string;
+  };
+  subData: {
+    name: string;
+    content: string;
+  };
 }
 interface StateProps {
-  Data:{
-    state : string;
-    pay : number;
-  }
+  Data: {
+    state: string;
+    pay: number;
+  };
 }
+
 const tempDataArr = [
   {
     id: 1,
-    state : WAIT_CONFIRM,
-    image : placeImg,
-    type : TEAM,
-    gymName : "사하 인피니트 스포츠",
-    location : "부산광역시 사하구 마하로48번길",
-    time : "2023.03.15(수) 15:00 ~ 16:30",
-    pay : 12000
-  }
-  ,
+    state: WAIT_CONFIRM,
+    image: placeImg,
+    type: TEAM,
+    gymName: '사하 인피니트 스포츠',
+    location: '부산광역시 사하구 마하로48번길',
+    time: '2023.03.15(수) 15:00 ~ 16:30',
+    pay: 12000,
+  },
   {
     id: 2,
-    state : WAIT_PAY,
-    image : placeImg,
-    type : TEAM,
-    gymName : "사하 인피니트 스포츠",
-    location : "부산광역시 사하구 마하로48번길",
-    time : "2023.03.15(수) 15:00 ~ 16:30",
-    pay : 13300
-  }
-  ,
+    state: WAIT_PAY,
+    image: placeImg,
+    type: TEAM,
+    gymName: '사하 인피니트 스포츠',
+    location: '부산광역시 사하구 마하로48번길',
+    time: '2023.03.15(수) 15:00 ~ 16:30',
+    pay: 13300,
+  },
   {
     id: 3,
-    state : RESERVE,
-    image : placeImg,
-    type : TEAM,
-    gymName : "사하 인피니트 스포츠",
-    location : "부산광역시 사하구 마하로48번길",
-    time : "2023.03.15(수) 15:00 ~ 16:30",
-    pay : 20000
-  }
-  ,
+    state: RESERVE,
+    image: placeImg,
+    type: TEAM,
+    gymName: '사하 인피니트 스포츠',
+    location: '부산광역시 사하구 마하로48번길',
+    time: '2023.03.15(수) 15:00 ~ 16:30',
+    pay: 20000,
+  },
   {
     id: 4,
-    state : WAIT_CONFIRM,
-    image : placeImg,
-    type : PICKUP,
-    gymName : "사하 인피니트 스포츠",
-    location : "부산광역시 사하구 마하로48번길",
-    time : "2023.03.15(수) 15:00 ~ 16:30",
-    pay : 19000
-  }
-  ,
+    state: WAIT_CONFIRM,
+    image: placeImg,
+    type: PICKUP,
+    gymName: '사하 인피니트 스포츠',
+    location: '부산광역시 사하구 마하로48번길',
+    time: '2023.03.15(수) 15:00 ~ 16:30',
+    pay: 19000,
+  },
   {
     id: 5,
-    state : RESERVE,
-    image : placeImg,
-    type : PICKUP,
-    gymName : "괴정 인피니트 스포츠",
-    location : "부산광역시 사하구 괴정로48번길",
-    time : "2023.03.17(수) 15:00 ~ 16:30",
-    pay : 17000
+    state: RESERVE,
+    image: placeImg,
+    type: PICKUP,
+    gymName: '괴정 인피니트 스포츠',
+    location: '부산광역시 사하구 괴정로48번길',
+    time: '2023.03.17(수) 15:00 ~ 16:30',
+    pay: 17000,
   },
   {
     id: 6,
-    state : RESERVE,
-    image : placeImg,
-    type : PICKUP,
-    gymName : "괴정 인피니트 스포츠",
-    location : "부산광역시 사하구 괴정로48번길",
-    time : "2023.03.17(수) 15:00 ~ 16:30",
-    pay : 17000
-  }
-  ,
+    state: RESERVE,
+    image: placeImg,
+    type: PICKUP,
+    gymName: '괴정 인피니트 스포츠',
+    location: '부산광역시 사하구 괴정로48번길',
+    time: '2023.03.17(수) 15:00 ~ 16:30',
+    pay: 17000,
+  },
   {
     id: 7,
-    state : WAIT_PAY,
-    image : placeImg,
-    type : TEAM,
-    gymName : "사하 인피니트 스포츠",
-    location : "부산광역시 사하구 마하로48번길",
-    time : "2023.03.15(수) 15:00 ~ 16:30",
-    pay : 13300
-  }
-]
-const ReserveState = ({state}:any) =>{
-  if(state === WAIT_CONFIRM){
-    return(
+    state: WAIT_PAY,
+    image: placeImg,
+    type: TEAM,
+    gymName: '사하 인피니트 스포츠',
+    location: '부산광역시 사하구 마하로48번길',
+    time: '2023.03.15(수) 15:00 ~ 16:30',
+    pay: 13300,
+  },
+];
+const ReserveState = ({ state }: any) => {
+  if (state === WAIT_CONFIRM) {
+    return (
       <>
-      <Notification>승인 대기중입니다.</Notification>
-      <Button>취소하기</Button>
-      </>)
-  }
-  else if(state === WAIT_PAY){
-    return(
+        <Notification>승인 대기중입니다.</Notification>
+        <Button>취소하기</Button>
+      </>
+    );
+  } else if (state === WAIT_PAY) {
+    return (
       <>
-      <Notification>결제가 완료되면 예약이 완료됩니다.</Notification>
-      <Button>결제하기</Button>
-      <Button>취소하기</Button>
-      </>)
-    
-  }
-  else if(state === RESERVE){
-    return(<Notification>예약이 완료되었습니다.</Notification>)
+        <Notification>결제가 완료되면 예약이 완료됩니다.</Notification>
+        <Button>결제하기</Button>
+        <Button>취소하기</Button>
+      </>
+    );
+  } else if (state === RESERVE) {
+    return <Notification>😊예약이 완료되었습니다.</Notification>;
   }
   return null;
-}
+};
 
-const Reserve = ({Data,subData}:Props) => {
+const Reserve = ({ Data, subData }: Props) => {
   return (
-    
     <Container>
-       {tempDataArr.map((el,index) => (
-              <ReserveInfoBox key = {index} className={Data.content === el.type && subData.content === el.state ? "active" : "unactive" }>
-                <Image src = {el.image} alt=""/>
-                <ReserveInfoList>
-                  <Title>{el.gymName}</Title>
-                  <Location>위치 : {el.location}</Location>
-                  <Time>{el.time}</Time>
-                  <ReserveState state = {el.state}/>
-                </ReserveInfoList>
-              </ReserveInfoBox>
-            ))}
+      {tempDataArr.map((el, index) => (
+        <ReserveInfoBox
+          key={index}
+          className={
+            Data.content === el.type && subData.content === el.state
+              ? 'active'
+              : 'unactive'
+          }
+        >
+          <Image src={el.image} alt="" />
+          <ReserveInfoList>
+            <Title>{el.gymName}</Title>
+            <Location>위치 : {el.location}</Location>
+            <Time>{el.time}</Time>
+            <ReserveState state={el.state} />
+          </ReserveInfoList>
+        </ReserveInfoBox>
+      ))}
     </Container>
-  )
-}
+  );
+};
 
-export default Reserve
-
+export default Reserve;
 
 const Container = styled.div`
   display: flex;
-  flex-direction : column;
-  list-style:none;
-  .unactive{
-    display :none;
+  flex-direction: column;
+  list-style: none;
+  .unactive {
+    display: none;
   }
 `;
 const Image = styled.img`
-@media ${({ theme }) => theme.grid.tablet} {
-  width : 35%;
-  display:block;
-}
-width : 70%;
-`
-const ReserveInfoBox = styled.div`
-  margin-bottom : 20px;
-  display : flex;
-  flex-direction : column;
-  @media ${({ theme }) => theme.grid.tablet}{
-    flex-direction : row;
+  @media ${({ theme }) => theme.grid.tablet} {
+    width: 35%;
+    display: block;
   }
-  
-`
+  width: 70%;
+`;
+const ReserveInfoBox = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  @media ${({ theme }) => theme.grid.tablet} {
+    flex-direction: row;
+  }
+`;
 
 const ReserveInfoList = styled.ul`
-  margin : 0px;
-  padding : 0px;
-  @media ${({ theme }) => theme.grid.tablet}{
-    flex-direction : row;
-    padding : 15px;
+  margin: 0px;
+  padding: 0px;
+  @media ${({ theme }) => theme.grid.tablet} {
+    flex-direction: row;
+    padding: 15px;
   }
-  list-style:none;
-`
+  list-style: none;
+`;
 
 const Title = styled.li`
-  font-size : 20px;
-  font-weight :bold;
-  margin-bottom : 5px;
-`
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 5px;
+`;
 const Location = styled.li`
-  font-size : 14px;
-`
+  font-size: 14px;
+`;
 
 const Time = styled.li`
-  margin : 20px 0px;
-`
+  margin: 20px 0px;
+`;
 const Pay = styled.li`
-  font-size : 24px;
-  font-weight : bold;
-`
+  font-size: 24px;
+  font-weight: bold;
+`;
 
 const Notification = styled.li`
-  margin : 10px 0px;
-`
+  margin: 10px 0px;
+`;
 
 const Button = styled.div`
-  padding : 10px 20px;
-  color : blue;
-  border : 1px solid blue;
-  text-align : center;
-  margin-bottom : 10px;
+  padding: 10px 20px;
+  color: blue;
+  border: 1px solid blue;
+  text-align: center;
+  margin-bottom: 10px;
 
-  &:hover{
-    background-color : blue;
-    color : white;
+  &:hover {
+    background-color: blue;
+    color: white;
   }
-`
+`;
