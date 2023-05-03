1e98b97f30cb74b2a2198a497fe64d29a3099fe9
daehwan2 <dhdh0482@naver.com>
2023-05-03 18:58:47 +0900

    mixin 생성

---
+++ src/styles/mixin/index.ts
@@ -0,0 +1,31 @@
+import { css } from 'styled-components';
+
+export const H5 = css`
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 1.5;
+`;
+
+export const H4 = css`
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 1.4;
+`;
+
+export const H3 = css`
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 1.3333333333333333;
+`;
+
+export const H2 = css`
+  font-weight: 600;
+  font-size: 30px;
+  line-height: 1.2666666666666666;
+`;
+
+export const H1 = css`
+  font-weight: 600;
+  font-size: 38px;
+  line-height: 1.2105263157894737;
+`;
