aa9cc15c5f9b7258d388580d52178a5292abbf3d
daehwan2 <dhdh0482@naver.com>
2023-06-01 00:40:02 +0900

    슬라이드 이미지 임시 변경

--- src/container/index.tsx
+++ src/container/index.tsx
@@ -3,6 +3,11 @@ import Slide from 'components/Slide';
 import React from 'react';
 import temp1 from 'assets/temp1.png';
 import temp2 from 'assets/temp2.png';
+import temp3 from 'assets/temp3.png';
+import temp4 from 'assets/temp4.png';
+import temp5 from 'assets/temp5.png';
+import temp6 from 'assets/temp6.png';
+import temp7 from 'assets/temp7.png';
 // import TeamCreateModal from 'components/mypage/TeamCreateModal';
 // import GymCreateModal from 'components/common/GymCreateModal';
 // import Landing from 'components/home/Landing';
@@ -13,11 +18,23 @@ import styled from 'styled-components';
 const tempDataArr = [
   {
     id: 1,
-    image: temp1,
+    image: temp3,
   },
   {
     id: 2,
-    image: temp2,
+    image: temp4,
+  },
+  {
+    id: 3,
+    image: temp5,
+  },
+  {
+    id: 4,
+    image: temp6,
+  },
+  {
+    id: 5,
+    image: temp7,
   },
 ];
 
